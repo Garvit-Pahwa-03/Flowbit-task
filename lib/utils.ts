690231554be51532e8c870975985fc7bb9ce8186
Garvit Pahwa <pahwagarvit775@gmail.com>
2025-11-10 18:55:35 +0530

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export const getBaseURL = () => {
+  // If the code is running on the server, use the VERCEL_URL.
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+  
+  // If the code is running in the browser, it's relative to the current domain.
+  // Fallback to localhost for local development.
+  return process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
+};
