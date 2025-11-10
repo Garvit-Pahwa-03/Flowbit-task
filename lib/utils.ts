91f06d0e35bb39d30eeb2d6be57f12e816502390
Garvit Pahwa <pahwagarvit775@gmail.com>
2025-11-10 19:06:06 +0530

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,22 @@
-import { clsx, type ClassValue } from "clsx"
+import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const getBaseURL = (): string => {
+  if (typeof window !== 'undefined') {
+    return '';
+  }
+
+  if (process.env.NEXT_PUBLIC_APP_URL) {
+    return process.env.NEXT_PUBLIC_APP_URL;
+  }
+
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  return 'http://localhost:3000';
+};
